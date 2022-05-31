0363e36ed7e615e9f61d0a60406e494ce6013d25
andromeda <andromeda@Andromeda.local>
2022-05-30 23:18:38 -0400

    Add random background colors to NFTs

--- contracts/epicNFT.sol
+++ contracts/epicNFT.sol
@@ -15,15 +15,17 @@ contract EpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-  // So, we make a baseSvg variable here that all our NFTs can use.
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  
   // I create three arrays, each with their own theme of random words.
   // Pick some random funny words, names of anime characters, foods you like, whatever! 
   string[] firstWords = ["Lion", "Shark", "Lemur", "Elephant", "Cow", "Dog", "Lion", "Shark", "Lemur", "Elephant", "Cow", "Dog", "Lemur", "Elephant", "Cow", "Dog"];
   string[] secondWords = ["laughs", "breathes", "drinks", "crawls", "howls", "jumps", "laughs", "breathes", "drinks", "crawls", "howls", "jumps", "laughs", "breathes", "drinks", "crawls", "howls", "jumps"];
   string[] thirdWords = ["over", "heavily", "feverently", "quickly", "creatively", "cautiously", "over", "heavily", "feverently", "quickly", "creatively", "cautiously", "over", "heavily", "feverently", "quickly", "creatively", "cautiously"];
+  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+  
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   constructor() ERC721 ("CircleNFT", "CIRCLE") {
     console.log("Contract constructed!");
@@ -53,6 +55,12 @@ contract EpicNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
+  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
+
   function random(string memory input) internal pure returns (uint256) {
       return uint256(keccak256(abi.encodePacked(input)));
   }
@@ -64,8 +72,8 @@ contract EpicNFT is ERC721URIStorage {
     string memory second = pickRandomSecondWord(newItemId);
     string memory third = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
-
-    string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+    string memory randomColor = pickRandomColor(newItemId);
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
     // Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
@@ -107,5 +115,8 @@ console.log("--------------------\n");
   
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+  
+    emit NewEpicNFTMinted(msg.sender, newItemId);
+
   }
 }
